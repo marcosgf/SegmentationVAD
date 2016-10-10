7beca2326e18578f8744f661e4d8a88a1cec9ee2
Marcos Valadão <marcosvgf95@gmail.com>
2016-10-09 23:53:09 -0300

    09/10/2016

--- segmentacao_VAD.php
+++ segmentacao_VAD.php
@@ -7,7 +7,8 @@
  */
 $limit_sec = 1;
 $arqs = scandir('tempos_VAD/',1);
-for($i = 0 ; $i < 1 ; $i++){
+for($i = 600 ; $i < sizeof($arqs) ; $i++){
+    echo " Arquivo atual: $arqs[$i] \n";
 
     //vetores de tempos originais e legendas
     $file = file("tempos/$arqs[$i]");
@@ -16,50 +17,63 @@ for($i = 0 ; $i < 1 ; $i++){
         $times[$k] = $file[$l+1];
         $k++;
     }
-   //var_dump($times);
     $k = 0 ;
     $file = file("legendas/$arqs[$i]");
     for($l = 0 ; $l < sizeof($file); $l = $l + 2){
         $legend[$k] = $file[$l+1];
         $k++;
     }
-    //var_dump($legend);
-
-    //testes
-    /*
-    $t_beg = (explode(" ", $times_vad[0]))[0];
-    $t_end = (explode(" ", $times_vad[0]))[1];
-    $end_ori = (explode(" ",$times[$id]))[1];
-    echo "$arqs[$i] \n \n".$t_beg."\n $t_end\n$end_ori \n";*/
 
     $times_vad = file("tempos_VAD/$arqs[$i]");
     $id = 0;
+    $l = 0;
     for($j = 0 ; $j < sizeof($times_vad) ; $j++){
-        echo "1 ";
+        $legenda = "";
+        //echo "1 ";
         $t_beg = (explode(" ", $times_vad[$j]))[0];
         $t_end = (explode(" ", $times_vad[$j]))[1];
         $end_ori = (explode(" ",$times[$id]))[1];
-        echo "2 ";
-        while($j < sizeof($times_vad)){
-            if(($t_end <= $end_ori && $end_ori <= $t_end+$limit_sec) || ($t_end-$limit_sec <= $end_ori && $end_ori <= $t_end)){
-                //anota legenda
-                //salva t_end
-                //proximo t_beg
-                echo "3 ";
+        //echo " t_beg = $t_beg ,  t_end = $t_end ,  end_ori = $end_ori \n";
+        while($j < sizeof($times_vad) && $id < sizeof($times)){
+            if(((float)$t_end <= (float)$end_ori && (float)$end_ori <= (float)$t_end+$limit_sec) ||
+                ((float)$t_end-$limit_sec <= (float)$end_ori && (float)$end_ori <= (float)$t_end)){
+                $leg_aux = $legenda;
+                //echo "2 ";
+                $legenda = str_replace("\n","",$leg_aux);
+                $legenda = $legenda.$legend[$id];
+                $id++;
                 break;
             }
-            elseif($end_ori > $t_end+$limit_sec){
-                //anota legenda
-                echo "4 ";
-                //proximo t_end
-                //proximo end_orig
+            elseif((float)$end_ori > (float)$t_end+$limit_sec || ((float)$t_end + $limit_sec) > (float)$end_ori){
                 $j++;
-                $id++;
+                if($j >= sizeof($times_vad)){
+                    $j--;
+                    //echo "3 ";
+                    $t_end = (explode(" ", $times_vad[$j]))[1];
+                    break;
+                }
                 $t_end = (explode(" ", $times_vad[$j]))[1];
-                $end_ori = (explode(" ",$times[$id]))[1];
+                if((float)$end_ori >= (float)$t_end || (((float)$t_end <= (float)$end_ori && (float)$end_ori <= (float)$t_end+$limit_sec) ||
+                        ((float)$t_end-$limit_sec <= (float)$end_ori && (float)$end_ori <= (float)$t_end))){
+                    //não anda com end_orig
+                    //echo "4 ";
+                }else{
+                    $id++;
+                    //echo "5 ";
+                    $leg_aux = $legenda;
+                    $legenda = str_replace("\n","",$leg_aux);
+                    $legenda = $legenda.$legend[$id];
+                    $end_ori = (explode(" ",$times[$id]))[1];
+                }
             }
         }
-        file_put_contents("testes.txt",$t_beg." ".$t_end."\n", FILE_APPEND);
+        //echo "6 ";
+        $name = str_pad(str_replace("A","",(explode(".",$arqs[$i]))[0]),3,0,STR_PAD_LEFT);
+        $id1 =  str_pad($l, 6, "0", STR_PAD_LEFT);
+        $begin = str_replace("\n","",str_pad((explode(".",$t_beg))[0].substr((explode(".",$t_beg))[1],0,-1), 6, "0",STR_PAD_LEFT));
+        $end = str_replace("\n","",str_pad((explode(".",$t_end))[0].substr((explode(".",$t_end))[1],0,-2), 6, "0",STR_PAD_LEFT));
+        file_put_contents("text","A$name-$id1-$begin-$end ".substr($t_beg,0,-1)." ".substr($t_end,0,-2)."\n", FILE_APPEND);
+        file_put_contents("segments","A$name-$id1-$begin-$end $legenda", FILE_APPEND);
+        $l++;
     }
-    //break;
 }
