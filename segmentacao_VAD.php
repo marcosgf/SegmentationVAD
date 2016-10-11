76f8c85b446246639a182b5be137b59f2e8eec70
Marcos Valadão <marcosvgf95@gmail.com>
2016-10-10 21:53:20 -0300

    10/10/2016

--- segmentacao_VAD.php
+++ segmentacao_VAD.php
@@ -7,7 +7,7 @@
  */
 $limit_sec = 1;
 $arqs = scandir('tempos_VAD/',1);
-for($i = 0 ; $i < sizeof($arqs) ; $i++){
+for($i = 0 ; $i < sizeof($arqs); $i++){
     echo " Arquivo atual: $arqs[$i] \n";
 
     //vetores de tempos originais e legendas
@@ -52,8 +52,11 @@ for($i = 0 ; $i < sizeof($arqs) ; $i++){
                     $t_end = (explode(" ", $times_vad[$j]))[1];
                     break;
                 }
-                if($t_end > $end_ori){
+                $aux_end_ori = (explode(" ",$times[$id + 1]))[1];
+                //echo " t_beg = $t_beg ,  t_end = $t_end ,  end_ori = $end_ori \n";
+                if((float)$t_end > (float)$end_ori && (float)$aux_end_ori < (float)$t_end){
                     $j--;
+                    //echo "6 ";
                 }
                 $t_end = (explode(" ", $times_vad[$j]))[1];
                 if((float)$end_ori >= (float)$t_end || (((float)$t_end <= (float)$end_ori && (float)$end_ori <= (float)$t_end+$limit_sec) ||
@@ -68,7 +71,7 @@ for($i = 0 ; $i < sizeof($arqs) ; $i++){
                     $legenda = $legenda.$legend[$id];
                     $end_ori = (explode(" ",$times[$id]))[1];
                 }
-                //echo " t_beg = $t_beg ,  t_end = $t_end ,  end_ori = $end_ori \n";
+
             }
         }
         //echo "6 ";
